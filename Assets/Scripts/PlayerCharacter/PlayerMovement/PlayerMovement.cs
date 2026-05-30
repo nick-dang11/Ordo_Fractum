283c2adfb638caf8befdd179130299de9ccd9cb1
Nick Dang <ndang7863@gmail.com>
2026-05-30 12:34:35 -0700

    PlayerMovement using new input system

---
+++ Assets/Scripts/PlayerCharacter/PlayerMovement/PlayerMovement.cs
@@ -0,0 +1,53 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class PlayerMovement : MonoBehaviour
+{
+    [SerializeField] private CharacterController _CharacterController;
+    [SerializeField] private Animator _Animator;
+    [SerializeField] private InputActionAsset _InputActions;
+
+    private float _gravity = -9.81f;
+    private Vector3 _velocity;
+    private InputAction _moveAction;
+
+    void Awake()
+    {
+        _CharacterController = GetComponent<CharacterController>();
+        _moveAction = _InputActions.FindActionMap("Player").FindAction("Move");
+    }
+
+    private void Update()
+    {
+        Vector2 input = _moveAction.ReadValue<Vector2>();
+
+        Vector3 direction = new Vector3(input.x, 0, input.y);
+
+        if(direction.magnitude > 0.1f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(direction);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 720f * Time.deltaTime); // 720f is two 360s, or two rotations per second, scalable
+
+            _CharacterController.Move(direction *5f * Time.deltaTime);
+        }
+
+        _Animator.SetFloat("Speed", direction.magnitude);
+
+        if (_CharacterController.isGrounded && _velocity.y < 0) {
+            _velocity.y = -2f;
+        }
+
+        _velocity.y += _gravity * Time.deltaTime;
+        _CharacterController.Move(_velocity * Time.deltaTime);
+    }
+
+    private void OnEnable()
+    {
+        _moveAction.Enable();
+    }
+
+    private void OnDisable()
+    {
+        _moveAction.Disable();
+    }
+}
