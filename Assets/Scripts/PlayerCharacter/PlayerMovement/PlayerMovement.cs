8b768479154d7746731fd191b869ecb72a0342ac
Nick Dang <ndang7863@gmail.com>
2026-06-02 16:54:14 -0700

    Feature: Cinemachine Freelook camera and camera relative movement

--- Assets/Scripts/PlayerCharacter/PlayerMovement/PlayerMovement.cs
+++ Assets/Scripts/PlayerCharacter/PlayerMovement/PlayerMovement.cs
@@ -7,6 +7,7 @@ public class PlayerMovement : MonoBehaviour
     [SerializeField] private CharacterController _CharacterController;
     [SerializeField] private Animator _Animator;
     [SerializeField] private InputActionAsset _InputActions;
+    [SerializeField] private Transform _MainCamera;
 
     [Header("Movement Settings")]
     [SerializeField] private float _moveSpeed = 2f;
@@ -21,23 +22,42 @@ public class PlayerMovement : MonoBehaviour
     {
         _CharacterController = GetComponent<CharacterController>();
         _moveAction = _InputActions.FindActionMap("Player").FindAction("Move");
+
+        if (_MainCamera == null && Camera.main != null)
+        {
+            Debug.LogWarning("Error: Main Camera may not be assigned to PlayerMovement.");
+            _MainCamera = Camera.main.transform;
+        }
     }
 
     private void Update()
     {
         Vector2 input = _moveAction.ReadValue<Vector2>();
 
-        Vector3 direction = new Vector3(input.x, 0, input.y);
+        Vector3 moveDirection = Vector3.zero;
 
-        if(direction.magnitude > 0.1f)
-        {
-            Quaternion targetRotation = Quaternion.LookRotation(direction);
-            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime); // 720f is two 360s, or two rotations per second, scalable
+        if (input.magnitude > 0.1f && _MainCamera != null) { 
+            Vector3 camForward = _MainCamera.forward;
+            Vector3 camRight = _MainCamera.right;
 
-            _CharacterController.Move(direction * _moveSpeed * Time.deltaTime);
+            camForward.y = 0f;
+            camRight.y = 0f;
+            camForward.Normalize();
+            camRight.Normalize();
+
+            moveDirection = (camForward * input.y) + (camRight * input.x);
         }
 
-        _Animator.SetFloat("Speed", direction.magnitude);
+
+        if(moveDirection.magnitude > 0.1f)
+        {
+            Quaternion targetRotation = Quaternion.LookRotation(moveDirection);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, _rotationSpeed * Time.deltaTime); // 720f is two 360s, or two rotations per second, scalable
+
+            _CharacterController.Move(moveDirection * _moveSpeed * Time.deltaTime);
+        }
+
+        _Animator.SetFloat("Speed", input.magnitude);
 
         if (_CharacterController.isGrounded && _velocity.y < 0) {
             _velocity.y = -2f;
