7d9431b28b7657204e44e026df81a3981a57666c
Nick Dang <ndang7863@gmail.com>
2026-08-08 11:59:08 -0700

    Feature: PlayerSheathSwordToggle
    Correctly enables and disables hip/hand swords for In/Out of combat. Animation events are very handy for firing off scripts at exact timelines.

---
+++ Assets/Scripts/PlayerCharacter/PlayerCombat/PlayerSheathSwordToggle.cs
@@ -0,0 +1,40 @@
+using UnityEngine;
+
+public class PlayerSheathSwordToggle : MonoBehaviour
+{
+    [SerializeField] private GameObject activeSword;
+    [SerializeField] private GameObject inactiveSword;
+
+    private void Start()
+    {
+        if(activeSword != null)
+        {
+            activeSword.SetActive(false);
+        }
+
+        if (inactiveSword != null)
+        {
+            inactiveSword.SetActive(true);
+        }
+    }
+
+    public void EquipSword()
+    {
+        Debug.Log("EquipSword called from animator event!");
+        if(inactiveSword != null && activeSword != null)
+        {
+            activeSword.SetActive(true);
+            inactiveSword.SetActive(false);
+        }
+    }
+    public void UnequipSword()
+    {
+        Debug.Log("UnequipSword called from animator event!");
+
+        if (inactiveSword != null && activeSword != null)
+        {
+            activeSword.SetActive(false);
+            inactiveSword.SetActive(true);
+        }
+    }
+}
