548019a0aafccc44d176c7450a446b345dfab8ca
Nick Dang <ndang7863@gmail.com>
2026-09-16 22:48:32 -0700

    Feature: Sprint 2.1 EnemyAttacks: EnemyWeaponHitbox
    Created EnemyWeaponHitbox as a subclass of WeaponHitbox abstract class. Largely similar to PlayerWeaponHitbox but requires changes to target detection and Health component to affect (PlayerHealth). Will require renaming and organizing of original health components created by Chris following approval.

---
+++ Assets/Scripts/Enemies/EnemyCombat/EnemyWeaponHitbox.cs
@@ -0,0 +1,29 @@
+using UnityEngine;
+
+public class EnemyWeaponHitbox : WeaponHitbox
+{
+    protected override bool IsValidTarget(Collider other)
+    {
+        return other.GetComponentInParent<HealthSystem>() != null;
+    }
+
+    protected override void HandleHit(Collider other)
+    {
+        HealthSystem healthSystem = other.GetComponentInParent<HealthSystem>();
+        if (healthSystem == null) return;
+
+        healthSystem.TakeDamage(Mathf.RoundToInt(CurrentDamage)); // rounding to prevent unforseen errors
+    }
+
+    [ContextMenu("DEBUG Enable Hitbox")]
+    private void DebugEnableHitbox()
+    {
+        EnableHitbox(1f);
+    }
+
+    [ContextMenu("DEBUG Disable Hitbox")]
+    private void DebugDisableHitbox()
+    {
+        DisableHitbox();
+    }
+}
